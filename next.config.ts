a38de0a46beec4c7f374dafe30eef9d6081fae90
Pavel Bezglasnyy <pavelb23@gmail.com>
2025-10-26 21:17:51 +0100

    update next js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+/*
 const nextConfig: NextConfig = {
-  /* config options here */
+   config options here 
+};
+*/
+
+const nextConfig = {
+  output: "export",
+  basePath: "cuda-indexer", // Change this to your repository name
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
